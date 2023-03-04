55219a3b51f169cce30167198a518daa9b73ab91
nathanmeade <nathan.r.meade@gmail.com>
2023-03-04 13:39:14 -0500

    Basic shift update functionality implemented

--- app/src/main/java/com/meadetechnologies/timetrackingapp/ui/shiftdetail/ShiftDetailActivity.kt
+++ app/src/main/java/com/meadetechnologies/timetrackingapp/ui/shiftdetail/ShiftDetailActivity.kt
@@ -2,15 +2,22 @@ package com.meadetechnologies.timetrackingapp.ui.shiftdetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import com.meadetechnologies.timetrackingapp.R
 import com.meadetechnologies.timetrackingapp.TimeTrackingDatabase
+import com.meadetechnologies.timetrackingapp.data.model.Shift
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ShiftDetailActivity : AppCompatActivity() {
 
     lateinit var timeTrackingDatabase: TimeTrackingDatabase
     lateinit var shiftInfoTextView: TextView
+    var shiftId : Int = 0
+    var employeeId : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shift_detail)
@@ -18,9 +25,18 @@ class ShiftDetailActivity : AppCompatActivity() {
         timeTrackingDatabase = TimeTrackingDatabase.getDatabase(this)
 
         val extras = intent.extras
-        val shiftId = extras?.getInt("shiftId") ?: 0
+        shiftId = extras?.getInt("shiftId") ?: 0
+        employeeId = extras?.getInt("employeeId") ?: 0
         timeTrackingDatabase.shiftDao().getShiftById(shiftId).observe(this, Observer {
             shiftInfoTextView.text = it.toString()
         })
+    }
+
+    fun update(view: View) {
+        val myScope = CoroutineScope(Dispatchers.IO)
+        myScope.launch {
+            timeTrackingDatabase.shiftDao().updateShift(Shift(shiftId, employeeId, Math.random().toString(), Math.random().toString()))
+
+        }
     }
 }
