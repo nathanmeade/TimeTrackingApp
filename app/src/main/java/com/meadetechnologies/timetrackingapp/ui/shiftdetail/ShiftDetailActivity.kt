372aca1823682de59cee82d244d503c7f9199fa4
nathanmeade <nathan.r.meade@gmail.com>
2023-03-04 13:32:03 -0500

    shift is now properly being passed into the ShiftDetailActivity

--- app/src/main/java/com/meadetechnologies/timetrackingapp/ui/shiftdetail/ShiftDetailActivity.kt
+++ app/src/main/java/com/meadetechnologies/timetrackingapp/ui/shiftdetail/ShiftDetailActivity.kt
@@ -2,11 +2,25 @@ package com.meadetechnologies.timetrackingapp.ui.shiftdetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.Observer
 import com.meadetechnologies.timetrackingapp.R
+import com.meadetechnologies.timetrackingapp.TimeTrackingDatabase
 
 class ShiftDetailActivity : AppCompatActivity() {
+
+    lateinit var timeTrackingDatabase: TimeTrackingDatabase
+    lateinit var shiftInfoTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shift_detail)
+        shiftInfoTextView = findViewById(R.id.shiftInfoTextView)
+        timeTrackingDatabase = TimeTrackingDatabase.getDatabase(this)
+
+        val extras = intent.extras
+        val shiftId = extras?.getInt("shiftId") ?: 0
+        timeTrackingDatabase.shiftDao().getShiftById(shiftId).observe(this, Observer {
+            shiftInfoTextView.text = it.toString()
+        })
     }
 }
