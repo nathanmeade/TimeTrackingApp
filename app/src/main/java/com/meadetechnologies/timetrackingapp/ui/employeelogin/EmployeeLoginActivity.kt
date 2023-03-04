47c7d2b241eea4ccd1ce7790d04f1228b26396e6
nathanmeade <nathan.r.meade@gmail.com>
2023-03-03 19:13:45 -0500

    Employee login navigation buttons implemented

--- app/src/main/java/com/meadetechnologies/timetrackingapp/ui/employeelogin/EmployeeLoginActivity.kt
+++ app/src/main/java/com/meadetechnologies/timetrackingapp/ui/employeelogin/EmployeeLoginActivity.kt
@@ -1,12 +1,26 @@
 package com.meadetechnologies.timetrackingapp.ui.employeelogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.meadetechnologies.timetrackingapp.R
+import com.meadetechnologies.timetrackingapp.ui.adminlogin.AdminLoginActivity
+import com.meadetechnologies.timetrackingapp.ui.timetracking.TimeTrackingActivity
 
 class EmployeeLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employee_login)
+    }
+
+    fun login(view: View) {
+        val loginIntent = Intent(this, TimeTrackingActivity::class.java)
+        startActivity(loginIntent)
+    }
+
+    fun navigateToAdminLogin(view: View) {
+        val navigateToAdminLoginIntent = Intent(this, AdminLoginActivity::class.java)
+        startActivity(navigateToAdminLoginIntent)
     }
 }
